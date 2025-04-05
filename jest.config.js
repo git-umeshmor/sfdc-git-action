beeb4d506acf6a62788a9723183aaeb4519409a8
Umesh <mailstoubmor@gmail.com>
2025-04-05 11:28:44 +0530

    Add package-lock.json

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,5 @@
 // jest.config.js
 module.exports = {
-  testMatch: [
-    "**/test/specs/**/*.[jt]s?(x)", // include your actual test path
-    "**/?(*.)+(spec|test).[tj]s?(x)"
-  ],
-  testPathIgnorePatterns: [
-    "/node_modules/"
-    // Remove this if it's blocking real tests
-    // "/test/specs/"
-  ]
+  testMatch: ["**/test/specs/**/*.+(js|ts|jsx|tsx)"],
+  testPathIgnorePatterns: ["/node_modules/"]
 };
